a4c6000ec1691a908522d45aba392a8fb38f8e13
Alduss <lettieri.aldo@gmail.com>
2013-09-28 20:36:25 +0200

    aggiunta gestione servo,cam,shutdown da pulsanti

--- chaser-man.c
+++ chaser-man.c
@@ -1,3 +1,4 @@
+
 /****************************************
  *  chaser-man
  *****************************************/
@@ -18,17 +19,19 @@
 #define DEVICE_ADDRESS 0x58
 
 //PIN FOR LEDS
+
 //GPIO7 - pin26
 #define  YELLOW_LED RPI_V2_GPIO_P1_26
 //GPIO8 - pin24
 #define  GREEN_LED RPI_V2_GPIO_P1_24
-//GPIO11pin23
+//GPIO11- pin23
 #define  WHITE_LED RPI_V2_GPIO_P1_23
 //GPIO25 - pin22 for detection of wiimotee camera
 #define  RED_LED RPI_V2_GPIO_P1_22
 
 
 //PIN FOR BUTTONS (FROM UP TO DOWN)
+
 //GPIO22 - PIN15 (BLACK BUTTON UP)
 #define FIRST_BUTTON_BLACK RPI_V2_GPIO_P1_15
 //GPIO27 - PIN13 (RED BUTTON)
@@ -47,26 +50,8 @@
 #define TRUE 1
 #define FALSE 0
 
-int init();
-void set_output_pin();
-void set_input_pin();
-uint8_t read_pin_status(int pin);
-void set_pin_high(int pin);
-void set_pin_low(int pin);
-void wait_a_bit();
-void init_wiicamera_registers();
-void detect_ir_led();
-
-
-void led_red_on();
-void led_red_off();
-void led_yellow_on();
-void led_yellow_off();
-void led_white_on();
-void led_white_off();
-void led_green_on();
-void led_green_off();
-
+//new structure type for all wiimote camera points 
+/***********************/
 struct ir_point{
 int x;
 int y;
@@ -79,10 +64,64 @@ struct ir_point p2;
 struct ir_point p3;
 struct ir_point p4;
 }t_points;
+/**********************/
 
+//new t_points declaration
 t_points points;
+
+//fd declaration for i2c bus
 int i2c_fd;
 
+int in_shutdown = FALSE; //TRUE start shutdown
+int video_mode = FALSE; // false video stop and true video on
+int servo_mode = FALSE; // FALSE  (x ok and y ok) - TRUE (x ok and y stop)
+
+int init();
+void shutdown();
+void reset_pos_servo();
+void set_output_pin();
+void set_input_pin();
+uint8_t read_pin_status(int pin);
+void set_pin_high(int pin);
+void set_pin_low(int pin);
+void wait_a_bit();
+void init_wiicamera_registers();
+void detect_ir_led();
+void move_servo(int num_servo,int value);
+int calculates_position(int *diff1,int *diff2, int *old_point_p1_x, int *old_point_p1_y,
+             int *old_pos_servo_x, int *old_pos_servo_y,t_points points,int *pos_servo_x,int *pos_servo_y);
+
+void led_red_on();
+void led_red_off();
+void led_yellow_on();
+void led_yellow_off();
+void led_white_on();
+void led_white_off();
+void led_green_on();
+void led_green_off();
+
+
+void  onoff(){
+	reset_pos_servo();
+        //led_yellow_off();
+	led_green_on();
+        reset_pos_servo();
+        sleep(1);
+        led_green_off();
+        led_white_on();
+        reset_pos_servo();
+        sleep(1);
+        //led_white_off();
+        led_white_off();
+	led_green_on();
+        reset_pos_servo();
+        sleep(1);
+	led_green_off();
+	led_white_on();
+	sleep(1);
+	led_white_off();
+        //led_red_off();
+}
 
 
 int is_detected_ir(int x,int y, int s){
@@ -95,30 +134,17 @@ int is_detected_ir(int x,int y, int s){
 	}
 }
 
-char move_servo(int num_servo,int value){
-char str[256];
-char n[4];
-char v[4];
-
+void move_servo(int num_servo,int value){
+	char str[256];
+	char n[4];
+	char v[4];
 	snprintf(n, sizeof(n), "%d", num_servo);
 	snprintf(v, sizeof(v), "%d", value);
-	strcpy(str,"sudo echo ");
-	strcat(str,n);
-	strcat(str,"=");
-	strcat(str,v);
+	strcpy(str,"sudo echo ");strcat(str,n);
+	strcat(str,"=");strcat(str,v);
 	strcat(str," > /dev/servoblaster");
-	printf("cmd servo:-->  %s",str);
+	printf("cmd servo:-->  %s\n",str);
 	system(str);
-	snprintf(v, sizeof(v), "%d", 0);
-	strcpy(str,"sudo echo ");
-	strcat(str,n);
-	strcat(str,"=");
-	strcat(str,v);
-	strcat(str," > /dev/servoblaster");
-	//printf("MOVE SERVO:  %s",str);
-	system(str);
-
-
 }
 
 
@@ -151,22 +177,30 @@ void led_white_off(){
         set_pin_low(WHITE_LED);
 }
 
-	
+
+void reset_pos_servo(){
+	move_servo(5,INITPOSY); //5 FOR Y
+        move_servo(5,0);
+        move_servo(6,INITPOSX); //6 FOR X
+        move_servo(6,0);
+}
+
+
 int init(){
 	if(bcm2835_init()){
+		system("sudo /home/pi/PiBits/ServoBlaster/servod --p1pins=0,0,0,0,16,18");
+		
 		set_output_pin();
 		set_input_pin();
-		system("sudo /home/pi/PiBits/ServoBlaster/servod --p1pins=0,0,0,0,16,18");
-		move_servo(5,INITPOSY); //5 FOR Y
-		move_servo(6,INITPOSX); //6 FOR X
-		//system("sudo -u pi ./streaming.sh &");
+		onoff();
 		init_wiicamera_registers();
-		printf("init ok...");
+		printf("init ok...\n");
+		sleep(2);
         	return 1;
 	}
 	else{
-		perror("init error! exit..");
-		return 1;
+		perror("init error! exit..\n");
+		return -1;
 	}
 
 }
@@ -181,17 +215,20 @@ void set_output_pin(){
 	bcm2835_gpio_write(WHITE_LED, LOW);
 	bcm2835_gpio_fsel(RED_LED, BCM2835_GPIO_FSEL_OUTP);
 	bcm2835_gpio_write(RED_LED, LOW);
-	printf("set output ok...");
+	printf("set output pin ok...\n");
 }
 
 void set_input_pin(){
 	bcm2835_gpio_fsel(FIRST_BUTTON_BLACK, BCM2835_GPIO_FSEL_INPT);
 	bcm2835_gpio_set_pud(FIRST_BUTTON_BLACK, BCM2835_GPIO_PUD_DOWN);
+	//wait_a_bit();
 	bcm2835_gpio_fsel(SECOND_BUTTON_RED, BCM2835_GPIO_FSEL_INPT);
+	//wait_a_bit();
 	bcm2835_gpio_set_pud(SECOND_BUTTON_RED, BCM2835_GPIO_PUD_DOWN);
 	bcm2835_gpio_fsel(THIRD_BUTTON_BLACK, BCM2835_GPIO_FSEL_INPT);
+	//wait_a_bit();
 	bcm2835_gpio_set_pud(THIRD_BUTTON_BLACK, BCM2835_GPIO_PUD_DOWN);
-	printf("set init ok...");
+	printf("set input pin ok...\n");
 }
 
 
@@ -199,6 +236,18 @@ uint8_t read_pin_status(int pin){
 	uint8_t value = bcm2835_gpio_lev(pin);
         printf("read from pin %d: %d\n",pin,value);
 	return value;
+}
+
+uint8_t status_button_shutdown(){
+	return read_pin_status(FIRST_BUTTON_BLACK);
+}
+
+uint8_t status_button_video(){
+	return read_pin_status(SECOND_BUTTON_RED);
+}
+
+uint8_t status_button_servo(){
+	return read_pin_status(THIRD_BUTTON_BLACK);
 }
 
 void set_pin_high(int pin){
@@ -221,7 +270,7 @@ int write2Byte(uint8_t first,uint8_t second){
         pkt_d[0] = first;
         pkt_d[1] = second;
         if(write(i2c_fd, &pkt_d, 2) != 2){
-                perror("write2Byte error!");
+                perror("write2Byte error!\n");
                 return -1;
         }
         return 1;
@@ -233,7 +282,7 @@ uint8_t readByte()
         pkt_d[0] =  0;
 
         if (read(i2c_fd, &pkt_d, 1) != 1) {
-                perror("read error in readByt");
+                perror("read error in readByte\n");
                 return -1;
         }
 
@@ -247,7 +296,7 @@ int writeByte(uint8_t value)
         pkt_d[0] = value;
 
         if (write(i2c_fd, &pkt_d, 1) != 1) {
-                perror("write error in writeByte");
+                perror("write error in writeByte\n");
                 return -1;
         }
 
@@ -279,72 +328,164 @@ void init_wiicamera_registers(){
         write2Byte(0x1A,0x40);sleep(0.01);
         write2Byte(0x33,0x33);sleep(0.01);
         sleep(0.1);
+	printf("wiimote camera registers ok...\n");
 }
 
 t_points get_points(){
 	uint8_t dataBuffer[16];
         int i = 0;
 	t_points points;
+	int x,y,s;
 
-	int Ix1,Ix2,Ix3,Ix4;
-	int Iy1,Iy2,Iy3,Iy4;
-	int s;
-	for(i=0;i<16;i++) {dataBuffer[i] = 0;}
+	memset(dataBuffer,0,sizeof(dataBuffer));
 	writeByte(0x36);
 	for(i=0;i<16;i++){dataBuffer[i] = readByte();}
-
-        Ix1 = dataBuffer[1];
-        Iy1 = dataBuffer[2];
-        s  = dataBuffer[3];
-	Ix1 += ( s & 0x30) <<4;
-        Iy1 += ( s & 0xC0) <<2; /*calcolo primo punto*/
-	points.p1.x = Ix1;
-        points.p1.y = Iy1;
-        points.p1.s = s;
-      
-	Ix2 = dataBuffer[4];
-        Iy2 = dataBuffer[5];
-        s = dataBuffer[6];
-        Ix2 +=(s & 0x30) <<4; /*calcolo secondo punto*/
-        Iy2 +=(s & 0xC0) <<2;
-	points.p2.x = Ix2;
-        points.p2.y = Iy2;
-        points.p2.s = s;
-
-        Ix3 = dataBuffer[7];
-        Iy3 = dataBuffer[8];
-        s = dataBuffer[9];
-        Ix3 += (s & 0x30) <<4;
-        Iy3 += (s & 0xC0) <<2; /*terzo punto*/
-	points.p3.x = Ix3;
-        points.p3.y = Iy3;
-        points.p3.s = s;
-	
-	Ix4 = dataBuffer[10];
-        Iy4 = dataBuffer[11];
-        s = dataBuffer[12];
-        Ix4 += (s & 0x30) <<4;
-        Iy4 += (s & 0xC0) <<2; /*quarto punto*/
-	points.p4.x = Ix4;
-        points.p4.y = Iy4;
-        points.p4.s = s;
-
+	points.p1.x = dataBuffer[1]+((dataBuffer[3] & 0x30) << 4);      
+	points.p1.y = dataBuffer[2]+((dataBuffer[3] & 0xC0) << 2);
+	points.p1.s = dataBuffer[3];
+	points.p2.x = dataBuffer[4]+((dataBuffer[6] & 0x30) << 4);      
+        points.p2.y = dataBuffer[5]+((dataBuffer[6] & 0xC0) << 2);
+        points.p2.s = dataBuffer[6];
+	points.p3.x = dataBuffer[7]+((dataBuffer[9] & 0x30) << 4);      
+        points.p3.y = dataBuffer[8]+((dataBuffer[9] & 0xC0) << 2);
+        points.p3.s = dataBuffer[9];
+	points.p4.x = dataBuffer[10]+((dataBuffer[12] & 0x30) << 4);      
+        points.p4.y = dataBuffer[11]+((dataBuffer[12] & 0xC0) << 2);
+        points.p4.s = dataBuffer[12];
 	return points;
 }
 
+int calculates_position(int *diff1,int *diff2, int *old_point_p1_x, int *old_point_p1_y,
+				int *old_pos_servo_x, int *old_pos_servo_y,t_points points,int *pos_servo_x,int *pos_servo_y){
+
+	*diff1 = abs(*old_point_p1_x-points.p1.x);
+        if(points.p1.x > 0 && points.p1.x < 500 && *diff1>10){
+		if(*diff1>10 && *diff1<=40)
+                	*pos_servo_x = *pos_servo_x - 1;
+                if(*diff1>40 &&  *diff1<=100)
+			*pos_servo_x = *pos_servo_x - 3;
+		if(*diff1>100)
+                       *pos_servo_x = *pos_servo_x - 5;
+		*old_point_p1_x = points.p1.x;
+	}
+	if(points.p1.x >=500 && points.p1.x < 1023 && *diff1>10){
+		if(*diff1>10 && *diff1<=40)
+                       *pos_servo_x = *pos_servo_x + 1;
+                if(*diff1>40 &&  *diff1<=100)
+                       * pos_servo_x = *pos_servo_x + 3;
+                if(*diff1>100)
+                       * pos_servo_x = *pos_servo_x + 5;
+                *old_point_p1_x = points.p1.x;
+        }
+
+	*diff2 = abs(*old_point_p1_y-points.p1.y);
+        if(points.p1.y > 0 && points.p1.y <= 362 && *diff2>10 &&!servo_mode){
+		if(*diff2>10 && *diff2<=40)
+               		*pos_servo_y = *pos_servo_y + 1;
+                if(*diff2>40 &&  *diff2<=100)
+                       	*pos_servo_y = *pos_servo_y + 3;
+                if(*diff2>100)
+                        *pos_servo_y = *pos_servo_y + 5;
+                *old_point_p1_y = points.p1.y;
+        }
+	if(points.p1.y  > 362 && points.p1.y <= 768 && *diff2>10 && !servo_mode){
+		if(*diff2>10 && *diff2<=40)
+               		*pos_servo_y = *pos_servo_y - 1;
+                if(*diff2>40 &&  *diff2<=100)
+                        *pos_servo_y = *pos_servo_y - 3;
+                if(*diff2>100)
+                        *pos_servo_y = *pos_servo_y - 5;
+                *old_point_p1_y = points.p1.y;
+        }
+	 
+	if(*pos_servo_y!=*old_pos_servo_y && !servo_mode){
+        	move_servo(5,(*pos_servo_y));
+       		move_servo(5,0);
+        }
+       	if(*pos_servo_x!=*old_pos_servo_x){
+                move_servo(6,(*pos_servo_x));
+                move_servo(6,0);
+        }
+                        
+	*old_pos_servo_x = *pos_servo_x;
+        *old_pos_servo_y = *pos_servo_y;
+}
+
+
+
+void  shutdown(){
+	//bcm2835_set_debug(1);
+	uint8_t status = status_button_shutdown();
+	printf("status shutdown: %d\n",status);
+	// bcm2835_set_debug(0);
+	if(status || in_shutdown){
+	sleep(1);
+		if(!in_shutdown){
+			sleep(4);
+			system("sudo killall raspivid");
+			//system("sudo killall  servod");
+			close(i2c_fd);
+		} 
+		
+		onoff();
+		if(!in_shutdown) system("sudo init 0"); 
+		in_shutdown=TRUE;
+		
+
+	}
+ }
+
+
+void  mode_video(){
+        //bcm2835_set_debug(1);
+        uint8_t status = status_button_video();
+        printf("status video: %d\n",status);
+	if(status){
+		sleep(1);
+		if(video_mode){
+			led_white_off();
+			system("sudo  killall raspivid");
+			video_mode = FALSE;
+		}else{
+			led_white_on();
+			system("sudo -u pi ./streaming.sh &");
+			video_mode = TRUE;
+		}
+	}
+}
+
+
+void  mode_servo(){
+	uint8_t status = status_button_servo();
+        printf("status servo y: %d\n",status);
+        if(status){
+                sleep(1);
+                if(servo_mode){
+                        led_green_off();
+                        servo_mode = FALSE;
+                }else{
+                        led_green_on();
+                        servo_mode = TRUE;
+                }
+        }
+
+
+
+}
 
 
 int main(int argc, char* argv[])
 {
 
-    int val,diff1,diff2;
+    int diff1,diff2;
     int pos_servo_x = INITPOSX;
     int pos_servo_y = INITPOSY;
     int old_point_p1_x;
     int old_point_p1_y;
     int old_pos_servo_x;
     int old_pos_servo_y;
-	
+    t_points points;
+
         if(argc <= 1){
                 printf("too few args, try %s /dev/i2c-0\n",argv[0]);
                 return -1;
@@ -356,71 +497,27 @@ int main(int argc, char* argv[])
         }
 	
 	init();
-        //uint8_t dataBuffer[16];
-        //int i = 0;
-	t_points points;
+       led_yellow_on();
         while (1)
         {
+		shutdown();
+		mode_video();
+		mode_servo();
+		if(!in_shutdown){
+			points = get_points();
+                	printf("x1: %i\n",points.p1.x);
+                	printf("y1: %i\n",points.p1.y);
+			printf("s1: %i\n",points.p1.s);
+			if(is_detected_ir(points.p1.x,points.p1.y,points.p1.s)){
+				printf("ir detected!!\n");
+				calculates_position(&diff1,&diff2,&old_point_p1_x,&old_point_p1_y,
+					&old_pos_servo_x,&old_pos_servo_y,points,&pos_servo_x,&pos_servo_y);
 
-		//detect_ir_led();
-		points = get_points();
-                printf("x1: %i\n",points.p1.x);
-                printf("y1: %i\n",points.p1.y);
-		printf("s1: %i\n",points.p1.s);
-		if(is_detected_ir(points.p1.x,points.p1.y,points.p1.s)){
-			printf("ir detected!!\n");
-			diff1 = abs(old_point_p1_x-points.p1.x);
-			if(points.p1.x > 0 && points.p1.x < 500 && diff1>10){
-                                	
-					if(diff1>10 && diff1<=40)
-                        			pos_servo_x = pos_servo_x - 1;
-					if(diff1>40 &&  diff1<=100)
-						pos_servo_x = pos_servo_x - 3;
-					if(diff1>100)
-						pos_servo_x = pos_servo_x - 5;
-					old_point_p1_x = points.p1.x;
-
+			
+                                                
+			}else{
+				printf("ir not detected!!\n");
 			}
-			if(points.p1.x >=500 && points.p1.x < 1023 && diff1>10){
-                                
-					if(diff1>10 && diff1<=40)
-                                                pos_servo_x = pos_servo_x + 1;
-                                        if(diff1>40 &&  diff1<=100)
-                                                pos_servo_x = pos_servo_x + 3;
-                                       if(diff1>100)
-                                                pos_servo_x = pos_servo_x + 5;
-					old_point_p1_x = points.p1.x;
-			}
-
-			diff2 = abs(old_point_p1_y-points.p1.y);
-			if(points.p1.y > 0 && points.p1.y <= 362 && diff2>10){
-					
-					if(diff2>10 && diff2<=40)
-                                                pos_servo_y = pos_servo_y + 1;
-                                        if(diff2>40 &&  diff2<=100)
-                                                pos_servo_y = pos_servo_y + 3;
-                                        if(diff2>100)
-                                                pos_servo_y = pos_servo_y + 5;
-					old_point_p1_y = points.p1.y;
-			}
-			if(points.p1.y  > 362 && points.p1.y <= 768 && diff2>10){
-					
-					 if(diff2>10 && diff2<=40)
-                                                pos_servo_y = pos_servo_y - 1;
-                                        if(diff2>40 &&  diff2<=100)
-                                                pos_servo_y = pos_servo_y - 3;
-                                       if(diff2>100)
-                                                pos_servo_y = pos_servo_y - 5;
-					old_point_p1_y = points.p1.y;
-			}
-			if(pos_servo_y!=old_pos_servo_y)
-				move_servo(5,pos_servo_y);
-			if(pos_servo_x!=old_pos_servo_x)
-				move_servo(6,pos_servo_x);
-			old_pos_servo_x = pos_servo_x;
-			old_pos_servo_y = pos_servo_y;
-		}else{
-			printf("ir not detected!!\n");
 		}
                 sleep(0.5);
          }
